5471e5e34e6707081d357e512dacf83bb1df4970
PaladinStore <68577693+PaladinStore@users.noreply.github.com>
2020-07-22 14:32:36 -0600

    Update AppDelegate.m

--- 32 bit only iOS 9.3.5 Jailbreak/AppDelegate.m
+++ 32 bit only iOS 9.3.5 Jailbreak/AppDelegate.m
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.m
-//  32 bit only iOS 9.3.5 Jailbreak
+//  32 and 64 bits only iOS 13.5 Jailbreak
 //
-//  Created by Brandon Withall on 2017-02-14.
-//  Copyright © 2017 Brando1235703. All rights reserved.
+//  Created by PaladinStore on 2020-22-7.
+//  Copyright © 2020 PaladinStore. All rights reserved.
 //
 
 #import "AppDelegate.h"
