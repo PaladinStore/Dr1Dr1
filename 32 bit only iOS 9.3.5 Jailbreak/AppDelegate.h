c3661c87dcc2c19f60024e49a1f5c39d39d87ec1
PaladinStore <68577693+PaladinStore@users.noreply.github.com>
2020-07-22 13:52:56 -0600

    Update AppDelegate.h

--- 32 bit only iOS 9.3.5 Jailbreak/AppDelegate.h
+++ 32 bit only iOS 9.3.5 Jailbreak/AppDelegate.h
@@ -1,9 +1,9 @@
 //
 //  AppDelegate.h
-//  32 bit only iOS 9.3.5 Jailbreak
+//  32 bit only iOS 13.5 Jailbreak
 //
-//  Created by Brandon Withall on 2017-02-14.
-//  Copyright © 2017 Brando1235703. All rights reserved.
+//  Created by PaladinStore on 2020-22-7.
+//  Copyright © 2020 PalandinStore. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
